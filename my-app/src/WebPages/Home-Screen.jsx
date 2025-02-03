1c1da6d0c81732a18342484a3472082a2e7dd770
LoganJanes <lmjanes@mun.ca>
2025-02-03 15:08:30 -0330

    Home Page Design
    Designs the homepage to a basic style

--- my-app/src/WebPages/Home-Screen.jsx
+++ my-app/src/WebPages/Home-Screen.jsx
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import "./Home-Screen.css";
 
 const Home = () => {
+  const [city, setCity] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!city) {
+      setMessage("Please enter a city name.");
+    } else {
+      setMessage(""); // For error message handling.
+      console.log(`City: ${city}`); // Will Replace with API.
+    }
+  };
+
   return (
     <div>
       <section className="top-banner">
         <div className="container">
-          <h1 className="heading">Simple Weather App</h1>
-          <form>
-            <input type="text" placeholder="Search for a city" autoFocus />
+          <h1 className="heading">Quick Lookup Weather App</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search for a city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              autoFocus
+            />
             <button type="submit">SUBMIT</button>
-            <span className="msg"></span>
+            <span className="msg">{message}</span>
           </form>
         </div>
       </section>
