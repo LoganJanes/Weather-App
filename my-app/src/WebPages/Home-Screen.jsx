faa7a42119085843578175a8b9c4622755f5e5e9
LoganJanes <lmjanes@mun.ca>
2025-02-03 14:27:01 -0330

    Home screen displays
    Displays proper home screen on home page.

--- my-app/src/WebPages/Home-Screen.jsx
+++ my-app/src/WebPages/Home-Screen.jsx
@@ -1,25 +1,25 @@
-
 import React, { useState } from "react";
 
 const Home = () => {
+  return (
     <div>
-<section class="top-banner">
-  <div class="container">
-    <h1 class="heading">Simple Weather App</h1>
-    <form>
-      <input type="text" placeholder="Search for a city" autofocus>
-      <button type="submit">SUBMIT</button>
-      <span class="msg"></span>
-      </input>
-    </form>
-  </div>
-</section>
-<section class="ajax-section">
-  <div class="container">
-    <ul class="cities"></ul>
-  </div>
-</section>
-</div>
+      <section className="top-banner">
+        <div className="container">
+          <h1 className="heading">Simple Weather App</h1>
+          <form>
+            <input type="text" placeholder="Search for a city" autoFocus />
+            <button type="submit">SUBMIT</button>
+            <span className="msg"></span>
+          </form>
+        </div>
+      </section>
+      <section className="ajax-section">
+        <div className="container">
+          <ul className="cities"></ul>
+        </div>
+      </section>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
