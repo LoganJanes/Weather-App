f1fbcccc28ad444f67c5e12f4680a83900c6da18
LoganJanes <lmjanes@mun.ca>
2025-02-03 14:17:26 -0330

    Getting homepage setup
    Setting up homepage to appear properly.

---
+++ my-app/src/WebPages/Home-Screen.jsx
@@ -0,0 +1,25 @@
+
+import React, { useState } from "react";
+
+const Home = () => {
+    <div>
+<section class="top-banner">
+  <div class="container">
+    <h1 class="heading">Simple Weather App</h1>
+    <form>
+      <input type="text" placeholder="Search for a city" autofocus>
+      <button type="submit">SUBMIT</button>
+      <span class="msg"></span>
+      </input>
+    </form>
+  </div>
+</section>
+<section class="ajax-section">
+  <div class="container">
+    <ul class="cities"></ul>
+  </div>
+</section>
+</div>
+};
+
+export default Home
